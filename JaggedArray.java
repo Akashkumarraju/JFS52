4a5b5f367b2ee9cce2847b579c071e8ae48f09bc
Akash <thangellaakashkumarraju@gmail.com>
2026-03-16 17:51:41 +0530

    Updated a JaggedArray code

--- JaggedArray.java
+++ JaggedArray.java
@@ -2,6 +2,18 @@ class JaggedArray
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		int[][] arr={
+			{23,35},
+			{20,30,40,50},
+			{60,70,90}
+		};
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.print(arr[i][j]+ " ");
+			}
+				System.out.println();
+		}
 	}
 }
